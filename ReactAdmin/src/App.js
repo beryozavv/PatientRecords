e2863cdf93a272c628386ef022a9e1f3219c47a1
ksa <karapetyan.sa@gmail.com>
2019-06-29 18:28:00 +0300

    Fix dataProvider

--- ReactAdmin/src/App.js
+++ ReactAdmin/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser, Edit, SimpleForm, TextInput, DateInput, TextField, LongTextInput, ShowGuesser } from "react-admin";
 import { SocialStatusList } from "./resources/socialStatus/SocialStatusList";
-import { dataProviderCreator } from "./dataProvider/dataProviderCreator";
+import dataProviderCreator from "./dataProvider/dataProviderCreator";
 // import { NewsEdit } from "./resources/news/NewsEdit";
 // import { CityList } from "./resources/cities/CityList";
 // import { CategoryList } from "./resources/category/CategoryList";
@@ -9,13 +9,10 @@ import { dataProviderCreator } from "./dataProvider/dataProviderCreator";
 
 // import { ExchangeList } from "./resources/exchange/ExchangeList";
 
-const dataProvider = dataProviderCreator(
-  "http://localhost:5000/api"
-);
-
+const dataProvider = dataProviderCreator("api");
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="SocialStatus" options={{ label: 'Социальный статус' }} list={SocialStatusList} />
+    <Resource name="SocialStatus" options={{ label: 'Социальный статус' }} list={ListGuesser} />
   </Admin>
 );
